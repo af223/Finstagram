2e44365c6e63ef654aa022869ed60deeba2cf1c9
af223 <aimee.feng24@gmail.com>
2021-07-06 19:15:12 -0400

    fetch posts

--- app/src/main/java/com/codepath/finstagram/MainActivity.java
+++ app/src/main/java/com/codepath/finstagram/MainActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
+import com.codepath.finstagram.models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "ActivityMain";
     private Button btnLogout;
+    private EditText etDescription;
+    private Button btnTakePic;
+    private Button btnPost;
+    private ImageView ivPicture;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +42,30 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        etDescription = findViewById(R.id.etDescription);
+        btnTakePic = findViewById(R.id.btnTakePic);
+        btnPost = findViewById(R.id.btnPost);
+        ivPicture = findViewById(R.id.ivPicture);
+
+        queryPosts();
     }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for(Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/codepath/finstagram/ParseApplication.java
+++ app/src/main/java/com/codepath/finstagram/ParseApplication.java
@@ -2,13 +2,18 @@ package com.codepath.finstagram;
 
 import android.app.Application;
 
+import com.codepath.finstagram.models.Post;
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Register parse model
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.app_id))
